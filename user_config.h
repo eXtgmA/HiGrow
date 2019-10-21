5424bb2a8dd5c3b4a70b75589704a891a28defce
eXtgmA <marvinkuenkler@gmail.com>
2019-10-21 08:08:58 +0200

    Update user_config.h
    Add MQTT Username and Password to user_config.h

--- user_config.h
+++ user_config.h
@@ -26,6 +26,8 @@
 #define MQTT_PORT               1883                  // [MQTT] MQTT Port on the Server
 #define MQTT_TOPIC              "smart/room/flower"   // [MQTT] Set the topic for your queue
 #define MQTT_INTERVAL           3600                  // [MQTT] Set the deepsleep time in seconds
+#define MQTT_USER                "username"           // [MQTT] Set a Username
+#define MQTT_PASSWORD            "***"                // [MQTT] Set a password
 
 
 // -- HTTP ---------------------------------------------- When webser is activ deepsleep will be disabled
